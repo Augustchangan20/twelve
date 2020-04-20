8db171763fb99a8fd093736d657486a90f8272af
Twelve <157513646@qq.com>
2020-04-20 21:27:36 +0800

    TestDemo

---
+++ untitled/2020-04-16/src/TestDemo.java
@@ -0,0 +1,71 @@
+
+    public class TestDemo{
+    public void add (int pos, int data){
+
+    }
+        public class MyArrayList {
+
+            public int[] elem;//数组
+            public int usedSize;//有效的数据个数
+            public static final int intCapacity = 10;//初始容量
+
+            public MyArrayList() {
+                this.elem = new int[intCapacity];
+                this.usedSize = 0;
+            }
+        }
+    }
+
+//
+//            public String toString() {
+//
+//                System.out.print("aaa");
+//
+//                return "bbb";
+//
+//            }
+//
+//
+//
+//
+//
+//
+//        public static void main(String[] args) {
+//
+//            System.out.println(new TestDemo());
+//        }
+//
+//    }
+////
+//            static int cnt = 6;
+//            static{
+//                cnt += 9;
+//            }
+//
+//        public static void main(String[] args) {
+//            System.out.println ("cnt=" + cnt);
+//        }
+//        static{
+//            cnt /=3;
+//        };
+//        }
+
+
+
+//        private int count;
+//
+//        public static void main(String[] args) {
+//
+//            TestDemo test=new TestDemo(88);
+//
+//            System.out.println(test.count);
+//
+//        }
+//
+//        TestDemo(int a) {
+//
+//            count=a;
+//
+//        }
+
+//    }
