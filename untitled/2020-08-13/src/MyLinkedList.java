75828219bf0a8e254297356a420f85a1de498c5d
Twelve <157513646@qq.com>
2020-08-17 23:42:11 +0800

    MyLinkedList

--- untitled/2020-08-13/src/MyLinkedList.java
+++ untitled/2020-08-13/src/MyLinkedList.java
@@ -1,4 +1,7 @@
 import com.sun.jmx.snmp.SnmpOidDatabase;
+import sun.applet.AppletResourceLoader;
+
+import java.math.BigDecimal;
 
 class Node{
     public int data;//数据
@@ -41,10 +44,37 @@ public class MyLinkedList {
         }
 
     }
-    //任意位置插入,第一个数据节点为0号下标
-    public boolean addIndex(int index,int data){
-        return false;
+    private void checkIndex(int index){
+        if (index < 0 || index > size()){
+            throw new RuntimeException("index不合法");
+        }
     }
+    private Node searchIndex(int index){
+        Node cur = this.head;
+        while (index != 0){
+            cur = cur.next;
+            index--;
+        }
+        return cur;
+    }
+    //任意位置插入,第一个数据节点为0号下标
+    public void addIndex(int index,int data){
+        checkIndex(index);//检查Index的合法性
+        if (index == 0){
+            addFirst(data);
+            return;
+    }
+        if (index == size()){
+            addLast(data);
+            return;
+    }
+    Node cur = searchIndex(index);//往中间插入
+    Node node = new Node(data);
+    node.next = cur;
+    node.prev = cur.prev;
+    cur.prev.next = node;
+    cur.prev = node;
+}
     //查找是否包含关键字key是否在单链表当中
     public boolean contains(int key) {
         Node cur = this.head;
@@ -57,12 +87,50 @@ public class MyLinkedList {
         return false;
     }
     //删除第一次出现关键字为key的节点
-    public void remove(int key){
-
+    public int remove(int key){
+        Node cur = this.head;//局部变量，函数结束就会消失
+        while (cur != null){
+            if (cur.data == key){
+                int oldDate = cur.data;
+                if (cur == this.head){//key为头节点
+                    this.head = this.head.next;
+                    this.head.prev = null;
+                }else {
+                    cur.prev.next = cur.next;
+                    if (cur.next != null){
+                        cur.next.prev = cur.prev;
+                    }else {
+                        //删除的是尾巴节点，只需要移动tail
+                        this.tail = cur.prev;
+                    }
+                }
+                return oldDate;
+            }
+            cur = cur.next;
+        }
+        return -1;
     }
     //删除所有值为key的节点
     public void removeAllKey(int key){
-
+        Node cur = this.head;
+        while (cur != null){
+            if (cur.data == key){
+                if (cur == this.head){
+                    this.head = this.head.next;
+                    if (cur.next != null){
+                        cur.prev = null;
+                    }
+                }else {
+                    cur.prev.next = cur.next;
+                    if (cur.next != null){
+                        cur.next.prev = cur.prev;
+                    }else {
+                        this.tail = cur.prev;
+                    }
+                }
+            }
+            cur = cur.next;
+        }
     }
     //得到单链表的长度
     public int size(){
@@ -84,7 +152,12 @@ public class MyLinkedList {
         System.out.println();
     }
     public void clear(){
-
+        while (this.head != null){
+            Node cur = this.head.next;
+            this.head.prev = null;
+            this.head.next = null;
+            this.head = cur;
+        }
+        this.tail = null;
     }
-
 }
--- untitled/2020-08-13/src/TeatDemo.java
+++ untitled/2020-08-13/src/TeatDemo.java
@@ -1,14 +1,32 @@
 public class TeatDemo {
     public static void main(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.addLast(11);
         myLinkedList.addLast(12);
-        myLinkedList.addLast(13);
-        myLinkedList.addLast(14);
+
+        myLinkedList.addLast(12);
+        myLinkedList.addLast(12);
+
+        myLinkedList.addLast(12);
         myLinkedList.display();
-        System.out.println(myLinkedList.contains(12));
+        /*System.out.println(myLinkedList.contains(12));
         System.out.println(myLinkedList.contains(15));
-        System.out.println(myLinkedList.size());
+        System.out.println(myLinkedList.size());*/
+        /*myLinkedList.addIndex(0,99);
+        myLinkedList.display();
+        myLinkedList.addIndex(5,66);
+        myLinkedList.display();
+        myLinkedList.addIndex(2,88);
+        myLinkedList.display();
+        myLinkedList.remove(99);
+        myLinkedList.display();
+        myLinkedList.remove(66);
+        myLinkedList.display();
+        myLinkedList.remove(88);
+        myLinkedList.display();*/
+     /*   myLinkedList.removeAllKey(12);
+        myLinkedList.display();*/
+        myLinkedList.clear();
+        System.out.println("2222");
 
 
     }
